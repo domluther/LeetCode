c2d0a018955886582b40f32eb16fa891fc31de23
domluther <dluther@gmail.com>
2024-06-09 13:24:03 +0100

    Update maximum-subarray.js

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,11 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
+
+// Uses a sliding window to solve it 
 var maxSubArray = function(nums) {
-    if (nums.length === 1) return nums[0]
     let max = nums[0]
     let current = 0;
     for (let i = 0; i < nums.length; i++){
+    // Try adding the current with the next item, if it's not bigger then the window restarts
     current = current + nums[i] > nums[i] ? current + nums[i] : nums[i];
         max = Math.max(current, max)
     }
