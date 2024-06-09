af8ddbc5e09745dc6463f6da2ade0641105e46a4
domluther <dluther@gmail.com>
2024-06-09 13:25:33 +0100

    Time: 74 ms (46.16%) | Memory: 57.9 MB (38.93%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.js
+++ 53-maximum-subarray/maximum-subarray.js
@@ -2,13 +2,13 @@
  * @param {number[]} nums
  * @return {number}
  */
-
-// Uses a sliding window to solve it 
 var maxSubArray = function(nums) {
     let max = nums[0]
     let current = 0;
     for (let i = 0; i < nums.length; i++){
-    // Try adding the current with the next item, if it's not bigger then the window restarts
+    // Update current sum by adding the ith element
+    // If the current sum becomes less than the ith element,
+    // reset current sum to the ith element
     current = current + nums[i] > nums[i] ? current + nums[i] : nums[i];
         max = Math.max(current, max)
     }
